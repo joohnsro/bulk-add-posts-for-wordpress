91416303ea91ed63fdddecc60bf1ede93a397306
Jonathan Oliveira <jonathansroliveira@gmail.com>
2020-05-05 10:59:21 -0300

    Checando datas e horários

--- views/scripts.js
+++ views/scripts.js
@@ -39,28 +39,83 @@ function addPostSketch( event ) {
     }
 
     parent.appendChild(newPost);
+    dateInputs.refresh();
+    timeInputs.refresh();
 }
 
-var dateInput = document.querySelector('.date');
+class CalendarInput {
+    constructor( selector ) {
+        this.selector = selector;
+        this.targets  = document.querySelectorAll( selector );
+        
+        this.refresh();
+    }
 
-dateInput.addEventListener('keydown', function( event ){
+    refresh() {
+        this.targets = document.querySelectorAll( this.selector );
+        
+        this.trackInputs( this.handleChange );
+    }
 
-    var element  = event.target,
-        value    = element.value.split("");
+    trackInputs( callback ) {
+        this.targets.forEach(function(item, key) {
 
-    value.map(function(item, index, value){
-        var res = needBeInt( item );
-        if ( !res && item != '/' ) {
-            delete value[index];
-        }
-    });
+            item.addEventListener( 'keyup', callback );
 
-    element.value =  value.join('');
+        });
+    }
 
-    function needBeInt( key ) {
-        if ( key.match(/\D/) ) return false;
+    handleChange( event ) {
+        var element  = event.target,
+            value    = element.value.split("");
 
-        return true
-    }    
+        value.map(function(item, index, value){
+            var res = ( item.match(/\D/) ) ? false : true;
 
-});
+            if ( !res ) {
+                if ( item == '/' && ( index == 2 || index == 5 ) ) return;
+                
+                delete value[index];
+            }
+
+            if ( item != '/' && ( index == 2 || index == 5 ) ) {
+
+                delete value[index];            
+            }
+        });
+
+        element.value =  value.join('');
+
+    }
+
+}
+
+class TimeInput extends CalendarInput {
+
+    handleChange( event ) {
+        var element  = event.target,
+            value    = element.value.split("");
+
+        value.map(function(item, index, value){
+            var res = ( item.match(/\D/) ) ? false : true;
+
+            if ( !res ) {
+                if ( item == ':' && ( index == 2 || index == 5 ) ) return;
+                
+                delete value[index];
+            }
+
+            if ( item != ':' && ( index == 2 || index == 5 ) ) {
+
+                delete value[index];            
+            }
+        });
+
+        element.value =  value.join('');
+
+    }
+
+}
+
+var dateInputs = new CalendarInput( '.date' );
+var timeInputs = new TimeInput( '.time' );
